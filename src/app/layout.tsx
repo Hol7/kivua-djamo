e7c3a2782eac8ba9fae0714f5ead56c9876f9d45
fabrown40 <fabrown40@gmail.com>
2025-01-18 22:22:30 +0100

    first commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,32 @@
+import type { Metadata } from "next";
+import "./globals.css";
+import localFont from 'next/font/local'
+
+const myFont = localFont({
+  weight: '100',
+  variable: '--font-FuturaCyrillicBook',
+  src: '../../public/fonts/FuturaCyrillicBold.ttf'
+ })
+
+
+
+export const metadata: Metadata = {
+  title: "Create Next App",
+  description: "Kivua djamo clone",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${myFont.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import NavBar from "@/components/navbar";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="p-20">
+      <NavBar/>
+     
+    </div>
+  );
+}
